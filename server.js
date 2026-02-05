a7ec70dd9d6980470f31b948cc2dfa3f43277fa8
KienTran <tkien5551@gmail.com>
2026-02-05 14:51:51 +0700

    ok

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+// Middleware để parse JSON
+app.use(express.json());
+
+// Route cơ bản
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
+app.get("/test-1", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+app.get("/test-2", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index2.html"));
+});
+app.get("/test-3", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index3.html"));
+});
+
+// Khởi động server
+app.listen(port, () => {
+  console.log(
+    `=====================http://localhost:${port}======================`,
+  );
+});
