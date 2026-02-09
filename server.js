b163f57a2acd54ac335fc2210d8d6c401c985ecf
KienTran <tkien5551@gmail.com>
2026-02-09 21:06:03 +0700

    chuyen doi du an

--- server.js
+++ server.js
@@ -10,18 +10,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "home.html"));
 });
-app.get("/test", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "test.html"));
-});
-app.get("/test-1", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-app.get("/test-2", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index2.html"));
-});
-app.get("/test-3", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index3.html"));
-});
 
 // Khởi động server
 app.listen(port, () => {
